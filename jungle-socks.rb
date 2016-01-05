7644e70dde3bf3593c5ec1f9ccd7b65a8fed06e9
Amit Sainani <asainani@bleacherreport.com>
2016-01-04 21:49:54 -0800

    change to rspec

--- jungle-socks.rb
+++ jungle-socks.rb
@@ -2,54 +2,50 @@
 
 require 'selenium-webdriver'
 require 'rspec/expectations'
-include RSpec::Matchers
+extend RSpec::Matchers
 
-def setup
-  @driver = Selenium::WebDriver.for :firefox
-end
-
-def teardown
-  @driver.quit
-end
-
-def run
-  setup
-  yield
-  teardown
-end
-
-run do
-  @driver.get 'https://jungle-socks.herokuapp.com'
-
-  expect(@driver.title).to eql 'JungleSocks'
-
-  h1 = @driver.find_element(css: 'h1')
-  expect(h1.text).to eql 'Welcome To Jungle Socks!'
-
-  p = @driver.find_element(css: 'p')
-  expect(p.text).to eql 'Please enter the quantiy of each kind of sock and then click the checkout button'
-  
-  def quantity(zebra, lion, elephant, giraffe)
-  	element = @driver.find_element(id: 'line_item_quantity_zebra').send_keys zebra
-  	element = @driver.find_element(id: 'line_item_quantity_lion').send_keys lion
-  	element = @driver.find_element(id: 'line_item_quantity_elephant').send_keys elephant
-  	element = @driver.find_element(id: 'line_item_quantity_giraffe').send_keys giraffe
+describe 'JungleSocks' do
+  before(:each) do
+    @driver = Selenium::WebDriver.for :firefox
   end
 
-  quantity(5, 0, 3, 5)
-
-  ship_to_state = @driver.find_element(name: 'state')
-  options = ship_to_state.find_elements(tag_name: 'option')
-  options.each do |states|
-#  puts states.text
-  states.click if states.text == 'California'
+  after(:each) do
+    @driver.quit
   end
 
-  element = @driver.find_element(name: 'commit').click
+  it 'succeeded' do
+    @driver.get 'https://jungle-socks.herokuapp.com'
 
-  h1 = @driver.find_element(css: 'h1')
-  expect(h1.text).to eql 'Please Confirm Your Order'
+    expect(@driver.title).to eql 'JungleSocks'
 
-  element = @driver.find_element(id: 'subtotal')
-  expect(element.text).to eql 5*13.00 + 0*20.00 + 3*35.00 + 5*17.00
+    h1 = @driver.find_element(css: 'h1')
+    expect(h1.text).to eql 'Welcome To Jungle Socks!'
+
+    p = @driver.find_element(css: 'p')
+    expect(p.text).to eql 'Please enter the quantiy of each kind of sock and then click the checkout button'
+
+    def quantity(zebra, lion, elephant, giraffe)
+      element = @driver.find_element(id: 'line_item_quantity_zebra').send_keys zebra
+      element = @driver.find_element(id: 'line_item_quantity_lion').send_keys lion
+      element = @driver.find_element(id: 'line_item_quantity_elephant').send_keys elephant
+      element = @driver.find_element(id: 'line_item_quantity_giraffe').send_keys giraffe
+    end
+
+    quantity(5, 0, 3, 5)
+
+    ship_to_state = @driver.find_element(name: 'state')
+    options = ship_to_state.find_elements(tag_name: 'option')
+    options.each do |states|
+    # puts states.text
+    states.click if states.text == 'California'
+    end
+
+    element = @driver.find_element(name: 'commit').submit
+
+    h1 = @driver.find_element(css: 'h1')
+    expect(h1.text).to eql 'Please Confirm Your Order'
+
+    element = @driver.find_element(id: 'subtotal')
+    expect(element.text).to eql 5*13.00 + 0*20.00 + 3*35.00 + 5*17.00
+  end
 end
